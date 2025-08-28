b1d3d394b998ccd28aea01620901a21db79be474
RiKaZaRai <41432142+RiKaZaRai@users.noreply.github.com>
2025-08-28 16:36:47 +0200

    Update RiKaZaraiSyliusProductDuplicationExtension.php

--- src/DependencyInjection/RiKaZaraiSyliusProductDuplicationExtension.php
+++ src/DependencyInjection/RiKaZaraiSyliusProductDuplicationExtension.php
@@ -17,7 +17,8 @@ final class RiKaZaraiSyliusProductDuplicationExtension extends Extension
         $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        // CHANGEMENT : nouveau chemin
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
         
         if ($config['enabled']) {
             $loader->load('services.xml');
